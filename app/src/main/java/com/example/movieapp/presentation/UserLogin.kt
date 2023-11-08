6b269d72196ad49242f562f0e900d1f7d89d3b12
Lauren Hahn <Lhahn24@gwu.edu>
2023-11-08 04:15:46 -0500

    create account button leads to create account screen

--- app/src/main/java/com/example/movieapp/presentation/UserLogin.kt
+++ app/src/main/java/com/example/movieapp/presentation/UserLogin.kt
@@ -1,12 +1,22 @@
 package com.example.movieapp.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.movieapp.R
 
 class UserLogin : AppCompatActivity() {
+    private lateinit var createAccountButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_layout)
+
+        // Create Account Button selected navigates to the signup screen:
+        createAccountButton = findViewById(R.id.createAccountButton)
+        createAccountButton.setOnClickListener {
+            val intent = Intent(this@UserLogin, UserSignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/movieapp/presentation/UserSignUp.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieapp.R
+
+class UserSignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup_layout)
+    }
+}
