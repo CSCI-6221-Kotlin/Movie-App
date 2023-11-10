2461543e083c5c95e75db3acf5fc37087f2fcf7d
Lauren Hahn <Lhahn24@gwu.edu>
2023-11-10 01:09:07 -0500

    able to display user's username

--- app/src/main/java/com/example/movieapp/presentation/UserAccount.kt
+++ app/src/main/java/com/example/movieapp/presentation/UserAccount.kt
@@ -2,23 +2,60 @@ package com.example.movieapp.presentation
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.movieapp.R
+import com.example.movieapp.firebase.UserDatabase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class UserAccount : AppCompatActivity() {
     private lateinit var logoutButton: ImageButton
+    private lateinit var usernameText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.account_layout)
 
+        // Connect to Firebase:
+        val firebaseAuth = FirebaseAuth.getInstance()
+        val firebaseDatabase = FirebaseDatabase.getInstance()
+
+        // Change username field to current user's username:
+        usernameText = findViewById(R.id.usernameText)
+        var currentUsername: String
+        val currentUserEmail = firebaseAuth.currentUser?.email
+        if (currentUserEmail != null) {
+            val currentUserEmailDB = currentUserEmail.replace(".", "")
+
+            val referenceFirebaseData = firebaseDatabase.getReference("Users/$currentUserEmailDB")
+            referenceFirebaseData.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    snapshot.children.forEach { childSnapshot: DataSnapshot ->
+                        val u: UserDatabase? = childSnapshot.getValue(UserDatabase::class.java)
+                        if (u != null) {
+                            currentUsername = u.username
+                            usernameText.text = currentUsername
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.d("MovieApp", "Unable to change welcome text!")
+                }
+            })
+        }
+
         // Logout button clicked:
         logoutButton = findViewById(R.id.logoutButton)
         logoutButton.setOnClickListener {
-            if (FirebaseAuth.getInstance().currentUser != null) {
-                FirebaseAuth.getInstance().signOut()
+            if (firebaseAuth.currentUser != null) {
+                firebaseAuth.signOut()
 
                 val intent = Intent(this@UserAccount, MainActivity::class.java)
                 startActivity(intent)
