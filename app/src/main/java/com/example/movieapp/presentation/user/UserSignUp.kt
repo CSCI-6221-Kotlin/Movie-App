d3a7fee484df21c24024245a3065f8aa8d215fe5
Lauren Hahn <Lhahn24@gwu.edu>
2023-11-13 01:16:25 -0500

    added username unique check and refactored a lot of the code

--- app/src/main/java/com/example/movieapp/presentation/user/UserSignUp.kt
+++ app/src/main/java/com/example/movieapp/presentation/user/UserSignUp.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -13,7 +14,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.movieapp.R
 import com.example.movieapp.firebase.UserDatabase
 import com.example.movieapp.presentation.MainActivity
-import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -27,9 +27,6 @@ class UserSignUp : AppCompatActivity() {
     private lateinit var passwordRetyped: EditText
     private lateinit var createAccountButton: Button
     private lateinit var createProgressBar: ProgressBar
-    private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var firebaseAnalytics: FirebaseAnalytics
-    private lateinit var firebaseDatabase: FirebaseDatabase
 
     private val textWatcher: TextWatcher = object: TextWatcher {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -69,89 +66,146 @@ class UserSignUp : AppCompatActivity() {
         createProgressBar = findViewById(R.id.createProgressBar)
         createProgressBar.visibility = View.INVISIBLE
 
-        // Connecting to Firebase:
-        firebaseAuth = FirebaseAuth.getInstance()
-        firebaseAnalytics = FirebaseAnalytics.getInstance(this@UserSignUp)
-        firebaseDatabase = FirebaseDatabase.getInstance()
         createAccountButton.setOnClickListener {
             val enteredUsername: String = username.text.toString()
             val enteredEmail: String = email.text.toString()
-            val enteredEmailDB = enteredEmail.replace(".", "")
             val enteredPassword: String = password.text.toString()
             val enteredPasswordRetyped: String = passwordRetyped.text.toString()
 
             // Make progress bar visible:
             createProgressBar.visibility = View.VISIBLE
 
-            // Verify that both passwords are equal:
-            if (enteredPassword.equals(enteredPasswordRetyped, true)) {
-                firebaseAuth
-                    .createUserWithEmailAndPassword(enteredEmail, enteredPassword)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            // Store email and password:
-                            val user = firebaseAuth.currentUser
-                            val toastMsg = "Sign up success as " + user!!.email
-                            Toast.makeText(this@UserSignUp, toastMsg, Toast.LENGTH_LONG).show()
-                            firebaseAnalytics.logEvent("SignUp_Success", null)
+            // Verify that the username is unique:
+            // If unique: It'll create the account only if email was not used previously
+            // and passwords are the same
+            verifyUniqueUsername(enteredUsername, enteredEmail, enteredPassword, enteredPasswordRetyped)
+        }
+    }
 
-                            // Store username that'll match to the email:
-                            val newUser = UserDatabase(username = enteredUsername)
-                            val existingUsers = mutableListOf<UserDatabase>()
-
-                            val referenceFirebaseData =
-                                firebaseDatabase.getReference("users/$enteredEmailDB")
-                            referenceFirebaseData.addValueEventListener(object : ValueEventListener {
-                                override fun onDataChange(snapshot: DataSnapshot) {
-                                    snapshot.children.forEach { childSnapshot: DataSnapshot ->
-                                        val u: UserDatabase? =
-                                            childSnapshot.getValue(UserDatabase::class.java)
-                                        if (u != null) {
-                                            if (!existingUsers.contains(u)) {
-                                                existingUsers.add(u)
-                                            }
-                                        }
-                                    }
-
-                                    if (!existingUsers.contains(newUser)) {
-                                        val referenceFirebase =
-                                            firebaseDatabase.getReference("Users/$enteredEmailDB")
-                                        referenceFirebase.push().setValue(newUser)
-                                    }
-                                }
-
-                                override fun onCancelled(error: DatabaseError) {
-                                    Toast.makeText(
-                                        this@UserSignUp,
-                                        "Failed to connect to database!",
-                                        Toast.LENGTH_LONG
-                                    ).show()
-                                }
-                            })
-
-                            // Make sure progress bar is invisible now:
-                            createProgressBar.visibility = View.INVISIBLE
-
-                            // Navigate to home screen:
-                            val intent = Intent(this@UserSignUp, MainActivity::class.java)
-                            startActivity(intent)
-                        } else {
-                            val exception: Exception? = task.exception
-                            val toastMsg = "Sign up failed!"
-
-                            Toast.makeText(this@UserSignUp, toastMsg, Toast.LENGTH_LONG).show()
-                            firebaseAnalytics.logEvent("SignUp_Failed: $exception", null)
-
-                            // Make sure progress bar is invisible now:
-                            createProgressBar.visibility = View.INVISIBLE
+    private fun verifyUniqueUsername (enteredUsername: String, enteredEmail : String, enteredPassword: String, enteredPasswordRetyped: String) {
+        var usernameUnique = true
+        if (enteredUsername.isNotEmpty()) {
+            val firebaseDatabase : FirebaseDatabase = FirebaseDatabase.getInstance()
+            val referenceFirebaseData = firebaseDatabase.getReference("AllUsernames")
+            referenceFirebaseData.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    snapshot.children.forEach { childSnapShot: DataSnapshot ->
+                        val u : UserDatabase? = childSnapShot.getValue(UserDatabase::class.java)
+                        if (u != null) {
+                            if (u.username.equals(enteredUsername, true)) {
+                                usernameUnique = false
+                            }
                         }
                     }
-            } else {
-                Toast.makeText(this@UserSignUp, "Please make sure passwords are the same!", Toast.LENGTH_SHORT).show()
 
-                // Make sure progress bar is invisible now:
-                createProgressBar.visibility = View.INVISIBLE
-            }
+                    if (usernameUnique) {
+                        if (verifyPasswords(enteredPassword, enteredPasswordRetyped)) {
+                            createAccount(enteredEmail, enteredPassword, enteredUsername)
+                            referenceFirebaseData.removeEventListener(this)
+                        } else {
+                            Toast.makeText(applicationContext, "Passwords are not the same. Please type again.", Toast.LENGTH_SHORT).show()
+                            createProgressBar.visibility = View.INVISIBLE
+                            referenceFirebaseData.removeEventListener(this)
+                        }
+                    } else {
+                        Toast.makeText(
+                            applicationContext,
+                            "$enteredUsername is already taken. Please type a different username.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        createProgressBar.visibility = View.INVISIBLE
+                        referenceFirebaseData.removeEventListener(this)
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.d("MovieApp", "Unable to identify usernames")
+                }
+            })
         }
+    }
+
+    private fun verifyPasswords(enteredPassword : String, enteredPasswordRetyped : String): Boolean {
+        return enteredPassword.equals(enteredPasswordRetyped, true)
+    }
+
+    private fun createAccount(enteredEmail : String, enteredPassword : String, enteredUsername: String) {
+        val firebaseAuth = FirebaseAuth.getInstance()
+        val firebaseDatabase = FirebaseDatabase.getInstance()
+
+        val enteredEmailDB = enteredEmail.replace(".", "")
+
+        firebaseAuth
+            .createUserWithEmailAndPassword(enteredEmail, enteredPassword)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val newUser = UserDatabase(username = enteredUsername)
+                    val user = firebaseAuth.currentUser
+
+                    // Store username in DB that'll match to the email:
+                    var referenceFirebaseData = firebaseDatabase.getReference("users/$enteredEmailDB")
+                    referenceFirebaseData.addValueEventListener(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            val referenceFirebase = firebaseDatabase.getReference("Users/$enteredEmailDB")
+                            referenceFirebase.push().setValue(newUser)
+
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                            Toast.makeText(
+                                this@UserSignUp,
+                                "Failed to connect to database!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    })
+
+                    // Store username to the DB that has all the usernames without the email as key:
+                    val existingUsers = mutableListOf<UserDatabase>()
+                    referenceFirebaseData = firebaseDatabase.getReference("AllUsernames")
+                    referenceFirebaseData.addValueEventListener(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            snapshot.children.forEach { childSnapshot: DataSnapshot ->
+                                val u: UserDatabase? =
+                                    childSnapshot.getValue(UserDatabase::class.java)
+                                if (u != null) {
+                                    if (!existingUsers.contains(u)) {
+                                        existingUsers.add(u)
+                                    }
+                                }
+                            }
+
+                            if (!existingUsers.contains(newUser)) {
+                                val referenceFirebase =
+                                    firebaseDatabase.getReference("AllUsernames")
+                                referenceFirebase.push().setValue(newUser)
+                            }
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                            Toast.makeText(
+                                this@UserSignUp,
+                                "Failed to connect to database!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    })
+
+                    // Make sure progress bar is invisible:
+                    createProgressBar.visibility = View.INVISIBLE
+
+                    // Navigate to home screen:
+                    val intent = Intent(this@UserSignUp, MainActivity::class.java)
+                    startActivity(intent)
+
+                    val toastMsg = "Sign up success as " + user!!.email
+                    Toast.makeText(this@UserSignUp, toastMsg, Toast.LENGTH_LONG).show()
+                } else {
+                    // Make sure progress bar is invisible:
+                    createProgressBar.visibility = View.INVISIBLE
+
+                    Toast.makeText(this@UserSignUp, "Creating account has failed with provided email and password!", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
